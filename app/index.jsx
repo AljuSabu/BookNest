1a3f45db84784ee0b35c2d2c5ef0f5db2c977ee2
Alju Sabu <aljusabu555@gmail.com>
2026-05-30 15:11:13 +0530

    pages and links added

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Logo from "../assets/images/favicon.png";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Hi My First App</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={styles.title}>The Number One</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>ReadingList App</Text>
+      <Link style={styles.link} href="/about">
+        About Page
+      </Link>
+
+      <Link style={styles.link} href="/contact">
+        Contact Page
+      </Link>
+    </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  img: {
+    marginVertical: 20,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
